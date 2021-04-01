d3d60c363db9e37c608d113fcd69d5dbca43419f
Gustavo <gustavo@pop-os.localdomain>
2021-04-01 16:09:32 -0300

    finding error on db conection

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -4,7 +4,7 @@ export default function users(req, res) {
     return new Promise((resolve, reject) => {
         knex('users')
             .then(results => {
-                res.json(results)
+                res.json({results: results})
                 resolve();
             })
             .catch(err => {
