18c2fd962d8bc4a8b4a166341c7ccdc9bcf12837
Gustavo <gustavo@pop-os.localdomain>
2021-04-01 15:49:38 -0300

    returning promises on api

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -1,5 +1,16 @@
 const knex = require('../../database')
 
 export default function users(req, res) {
-    knex('users').then((results) => res.json(results)) 
+    return new Promise((resolve, reject) => {
+        knex('users')
+            .then(results => {
+                res.json(results)
+                resolve();
+            })
+            .catch(err => {
+                res.json(error);
+                res.status(405).end();
+                return resolve()
+            })
+    })
 }
